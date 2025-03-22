4cc10bb118355eb20b4e552d7f2e4ebccb0a538b
Abdelmoughit Fikri <Abdelmoughit.fikri27@gmail.com>
2025-03-22 20:40:53 +0000

    PUSHII

--- heartinhealth/heartinhealth/settings/prod.py
+++ heartinhealth/heartinhealth/settings/prod.py
@@ -1,16 +1,16 @@
 from .base import *
 
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.mysql",
-#         "USER": os.environ.get('DB_USER_NAME'),
-#         "NAME": os.environ.get('DB_NAME'),
-#         "PASSWORD": os.environ.get('DB_PASSWORD'),
-#         "HOST": os.environ.get('PROD_DB_HOST'),
-#         "PORT": os.environ.get('PROD_DB_PORT',"3306"),
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "USER": os.environ.get('PROD_DB_USER_NAME'),
+        "NAME": os.environ.get('PROD_DB_NAME'),
+        "PASSWORD": os.environ.get('PROD_DB_PASSWORD'),
+        "HOST": os.environ.get('PROD_DB_HOST'),
+        "PORT": os.environ.get('PROD_DB_PORT',"3306"),
+    }
+}
 ALLOWED_HOSTS = [os.getenv('PROD_ALLOWED_HOST', '')]
 AWS_ACCESS_KEY_ID = os.environ.get('PROD_AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('PROD_AWS_SECRET_ACCESS_KEY')
