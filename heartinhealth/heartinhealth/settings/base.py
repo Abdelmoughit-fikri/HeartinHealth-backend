5ca08f5c51425b311da9df716772be4a78ba368d
Abdelmoughit Fikri <Abdelmoughit.fikri27@gmail.com>
2025-03-27 15:56:21 +0000

    sir

--- heartinhealth/heartinhealth/settings/base.py
+++ heartinhealth/heartinhealth/settings/base.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    'corsheaders.middleware.CorsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -63,10 +63,10 @@ TEMPLATES = [
     },
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://192.168.1.110:3000",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+# ]
+CORS_ALLOW_ALL_ORIGINS: True
 
 CORS_ALLOW_METHODS = [
     "GET",
