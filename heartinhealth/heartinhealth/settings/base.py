908f3c1077d36cbb6961d371306e35570b1d6b5a
Abdelmoughit Fikri <Abdelmoughit.fikri27@gmail.com>
2025-03-27 16:24:01 +0000

    lets see

--- heartinhealth/heartinhealth/settings/base.py
+++ heartinhealth/heartinhealth/settings/base.py
@@ -63,10 +63,9 @@ TEMPLATES = [
     },
 ]
 
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:3000",
-# ]
-CORS_ALLOW_ALL_ORIGINS: True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
 
 CORS_ALLOW_METHODS = [
     "GET",
@@ -75,7 +74,6 @@ CORS_ALLOW_METHODS = [
 
 CORS_ALLOW_HEADERS = [
     "Content-Type",
-    "X-Requested-With",
     "Accept",
     "Authorization",
 ]
